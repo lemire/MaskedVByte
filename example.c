4840c85cec2d6394f52d244b1af3f58515619db2
Daniel Lemire <lemire@gmail.com>
2014-11-19 14:52:11 -0500

    Progressively fixing things up.

--- example.c
+++ example.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "varintencode.h"
 #include "varintdecode.h"
 
 int main() {
+	simdvbyteinit(); // this must always be called once
 	int N = 5000;
 	uint32_t * datain = malloc(N * sizeof(uint32_t));
 	uint8_t * compressedbuffer = malloc(N * sizeof(uint32_t));
 	uint32_t * recovdata = malloc(N * sizeof(uint32_t));
 	for (int k = 0; k < N; ++k)
-		datain[k] = 140;
-	size_t compsize = vbyte_encode(datain, N, compressedbuffer);
-	masked_vbyte_decode(compressedbuffer, recovdata,
-					N);
+		datain[k] = 120;
+	size_t compsize = vbyte_encode(datain, N, compressedbuffer); // encoding
+	// here the result is stored in compressedbuffer using compsize bytes
+	size_t compsize2 = masked_vbyte_decode(compressedbuffer, recovdata,
+					N); // decoding (fast)
+	assert(compsize == compsize2);
 	free(datain);
 	free(compressedbuffer);
 	free(recovdata);
--- src/unit.c
+++ src/unit.c
@@ -5,37 +5,42 @@
 #include "varintdecode.h"
 
 int main() {
+	simdvbyteinit();
 	int N = 5000;
 	uint32_t * datain = malloc(N * sizeof(uint32_t));
 	uint8_t * compressedbuffer = malloc(N * sizeof(uint32_t));
 	uint32_t * recovdata = malloc(N * sizeof(uint32_t));
 
-	for (int length = 0; length <= N; ++length) {
+	for (int length = N; length <= N; ++length) {
+		printf("length = %d \n",length);
+
 		for (int gap = 1; gap <= 387420489; gap *= 3) {
+			printf("gap = %d \n",gap);
+
 			for (int k = 0; k < length; ++k)
 				datain[k] = gap;
 			size_t compsize = vbyte_encode(datain, length, compressedbuffer);
 			size_t usedbytes = masked_vbyte_decode(compressedbuffer, recovdata,
 					length);
 			if (compsize != usedbytes) {
-				printf("code is buggy");
+				printf("[masked_vbyte_decode] code is buggy, size mismatch %d %d \n",(int) compsize, (int) usedbytes);
 				return -1;
 			}
 			for (int k = 0; k < length; ++k) {
 				if (recovdata[k] != datain[k]) {
-					printf("code is buggy");
+					printf("[masked_vbyte_decode] code is buggy\n");
 					return -1;
 				}
 			}
 			size_t decodedints = masked_vbyte_decode_fromcompressedsize(compressedbuffer, recovdata,
 					compsize);
 			if (decodedints != (size_t) length) {
-				printf("code is buggy");
+				printf("[masked_vbyte_decode_fromcompressedsize] code is buggy %d %d \n",(int) compsize, (int) usedbytes);
 				return -1;
 			}
 			for (int k = 0; k < length; ++k) {
 				if (recovdata[k] != datain[k]) {
-					printf("code is buggy");
+					printf("[masked_vbyte_decode_fromcompressedsize] code is buggy\n");
 					return -1;
 				}
 			}
