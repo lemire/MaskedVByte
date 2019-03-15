a73ae2253c86214e8128f958defd0f1af33ea2f9
Daniel Lemire <lemire@gmail.com>
2019-03-15 11:55:52 -0400

    Removing misleading comment

--- include/varintencode.h
+++ include/varintencode.h
@@ -16,8 +16,6 @@ size_t vbyte_encode(uint32_t *in, size_t length, uint8_t *bout);
 // Encode an array of a given length read from in to bout in varint format with differential
 // coding starting at value prev. (Setting prev to 0 is a good default.)
 //
-// If the input is unsorted, then the result of the differential coding will be undefined as per the C spec.
-//
 // Returns the number of bytes written.
 size_t vbyte_encode_delta(uint32_t *in, size_t length, uint8_t *bout, uint32_t prev);
 
